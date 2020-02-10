696566d84ecae6ddbd39324951278ba8eb59beab
simcha23 <simcarmahlangu@gmail.com>
2020-02-10 11:49:25 +0200

    Tests modification

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -12,17 +12,14 @@ public class PersonTest {
                 "hardarse", "agile", "ssd hard drives"
         });
 
-        Assert.assertEquals(person.Hello(),"Hello, " +
-                "My name is Ryan and I am 30 years old. My interests are being a hardarse, agile and ssd hard drives.");
+//        Assert.assertEquals(person.Hello(),"Hello, " +
+//                "My name is Ryan and I am 30 years old. My interests are being a hardarse, agile and ssd hard drives.");
 
-
+        Assert.assertEquals(person.Hello(),"Hello, " + "My name is Ryan and I am 30 years old. My interests are being a hardarse, agile and ssd hard drives.");
+        Assert.assertEquals(person.getName(),"Ryan");
+        Assert.assertEquals(person.getAge(),30);
+        Assert.assertEquals(person.getGender(),"male");
+        Assert.assertEquals(person.getInterests(),new String[]{
+                "hardarse", "agile", "ssd hard drives"});
     }
-
-
-   // @Test
-//    public void testGetAge(){
-//        String interest[]={"1","2","3","4"};
-//        Person person = new Person("",30,"shemale",interest);
-//        Assert.assertEquals(person.add(2,3),5);
-//    }
 }
