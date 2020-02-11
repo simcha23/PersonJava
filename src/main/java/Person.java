7e5ed217b8d55a4e1f08b2769118c6e4ee920db7
simcha23 <simcarmahlangu@gmail.com>
2020-02-11 12:36:21 +0200

    update

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -3,8 +3,8 @@ public class Person {
     private String name;
     private int age;
     private String gender;
-    private String[] interests;
-    public Person(String name, int age, String gender, String[] interests) {
+    private ArrayList interests;
+    public Person(String name, int age, String gender, ArrayList interests) {
         this.name = name;
         this.age = age;
         this.gender = gender;
@@ -13,7 +13,7 @@ public class Person {
     public String getGender() {
         return gender;
     }
-    public String[] getInterests() {
+    public ArrayList getInterests() {
         return interests;
     }
     public String getName() {
@@ -22,18 +22,18 @@ public class Person {
     public int getAge() {
         return age;
     }
-    public String hello() {
+    public String Hello() {
+
         ArrayList last = new ArrayList();
-        for(int i=0; i<interests.length-1;i++){
-            last.add(interests[i]);
-        }
-        //assigning the last interest entered to a text string which i will use to display
-        String text = last.toString();
-        //The line above displays with square brackets
-        //hence i use a substring to get only the text and not the square brackets
-        text=text.substring(1,text.length()-1);
-        String txt="Hello, My name is "+ name+" and I am " + age +" years old." +
-                " My interests are being a " + text+ " "+ "and" + " " +(interests[interests.length-1])+".";
-        return txt;
+          for(int i=0; i<interests.size()-1;i++){
+              last.add(interests.get(i));
+          }
+
+        String items = last.toString();
+        items = items.substring(1,items.length()-1);
+
+        String output ="Hello, My name is " + name + " and I am " + age + " years old. My interests are being a " + items + " and " + interests.get(interests.size()-1) + ".";
+        return output;
+
     }
 }
--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -1,17 +1,48 @@
 import org.junit.Assert;
 import org.junit.Test;
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
 public class PersonTest {
+
     @Test
     public void testEmptyName(){
-        Person person = new Person("Ryan", 30, "male",new String[]{
-                "hardarse", "agile", "ssd hard drives"
-        });
-        Assert.assertEquals(person.hello(),"Hello, " +
+
+
+        ArrayList<String> interests =new ArrayList<>();
+
+        String firstName = JOptionPane.showInputDialog("Enter your Name");
+        String age = JOptionPane.showInputDialog("Enter your Age");
+        String gender = JOptionPane.showInputDialog("Enter your Gender");
+
+        int count = 1;
+        for(int x = 0 ; x <= interests.size() ; x++){
+
+            String name = JOptionPane.showInputDialog("Enter your interest " + count + " or press 'Stop' to terminate");
+
+            if(name.trim().equalsIgnoreCase("stop")){
+
+                break;
+            }
+            interests.add(name);
+            count++;
+        }
+
+        if(interests.size() == 0){
+            interests.add("no interests");
+        }
+
+        Person person=new Person(firstName, Integer.parseInt(age), gender, interests);
+
+        Assert.assertEquals(person.Hello(),"Hello, " +
                 "My name is Ryan and I am 30 years old. My interests are being a hardarse, agile and ssd hard drives.");
+
+        Assert.assertEquals(person.Hello(),"Hello, " + "My name is Ryan and I am 30 years old. My interests are being a hardarse, agile and ssd hard drives.");
         Assert.assertEquals(person.getName(),"Ryan");
         Assert.assertEquals(person.getAge(),30);
         Assert.assertEquals(person.getGender(),"male");
-        Assert.assertEquals(person.getInterests(),new String[]{
-                "hardarse", "agile", "ssd hard drives"});
+        Assert.assertEquals(person.getInterests(), Arrays.asList("hardarse, agile, ssd hard drives") );
     }
 }
