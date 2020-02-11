2a71c49d77fd516f0d9b7cd963a7d1479e147992
simcha23 <simcarmahlangu@gmail.com>
2020-02-11 11:02:43 +0200

    update

--- src/main/java/MainProgram.java
+++ src/main/java/MainProgram.java
@@ -1,10 +1,36 @@
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 public class MainProgram {
+    private static Object ArrayList;
+
     public static void main(String args[]) {
 
 
-        Person person=new Person("Ryan", 30, "male",new String[]{
-                "hardarse", "agile", "ssd hard drives"
-        });
+        ArrayList<String> interests =new ArrayList<>();
+
+        String firstName = JOptionPane.showInputDialog("Enter your Name");
+        String age = JOptionPane.showInputDialog("Enter your Age");
+        String gender = JOptionPane.showInputDialog("Enter your Gender");
+
+        int count = 1;
+        for(int x = 0 ; x <= interests.size() ; x++){
+
+            String name = JOptionPane.showInputDialog("Enter your interest " + count + " or press 'Stop' to terminate");
+
+            if(name.trim().equalsIgnoreCase("stop")){
+
+                break;
+            }
+            interests.add(name);
+            count++;
+        }
+
+        if(interests.size() == 0){
+            interests.add("no interests");
+        }
+
+        Person person=new Person(firstName, Integer.parseInt(age), gender, interests);
 
         System.out.println(person.Hello());
     }
--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -1,5 +1,8 @@
 import org.junit.Assert;
 import org.junit.Test;
+import javax.swing.*;
+import java.util.ArrayList;
+
 
 public class PersonTest {
 
@@ -7,19 +10,38 @@ public class PersonTest {
     public void testEmptyName(){
 
 
+        ArrayList<String> interests =new ArrayList<>();
 
-        Person person = new Person("Ryan", 30, "male",new String[]{
-                "hardarse", "agile", "ssd hard drives"
-        });
+        String firstName = JOptionPane.showInputDialog("Enter your Name");
+        String age = JOptionPane.showInputDialog("Enter your Age");
+        String gender = JOptionPane.showInputDialog("Enter your Gender");
 
-//        Assert.assertEquals(person.Hello(),"Hello, " +
-//                "My name is Ryan and I am 30 years old. My interests are being a hardarse, agile and ssd hard drives.");
+        int count = 1;
+        for(int x = 0 ; x <= interests.size() ; x++){
+
+            String name = JOptionPane.showInputDialog("Enter your interest " + count + " or press 'Stop' to terminate");
+
+            if(name.trim().equalsIgnoreCase("stop")){
+
+                break;
+            }
+            interests.add(name);
+            count++;
+        }
+
+        if(interests.size() == 0){
+            interests.add("no interests");
+        }
+
+        Person person=new Person(firstName, Integer.parseInt(age), gender, interests);
+
+        Assert.assertEquals(person.Hello(),"Hello, " +
+                "My name is Ryan and I am 30 years old. My interests are being a hardarse, agile and ssd hard drives.");
 
         Assert.assertEquals(person.Hello(),"Hello, " + "My name is Ryan and I am 30 years old. My interests are being a hardarse, agile and ssd hard drives.");
         Assert.assertEquals(person.getName(),"Ryan");
         Assert.assertEquals(person.getAge(),30);
         Assert.assertEquals(person.getGender(),"male");
-        Assert.assertEquals(person.getInterests(),new String[]{
-                "hardarse", "agile", "ssd hard drives"});
+//        Assert.assertEquals(person.getInterests(), Arrays.asList("hardarse, agile, ssd hard drives") );
     }
 }
